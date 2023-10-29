40b55fce2c07f1d1c0841d3e64abdf3dc445430d
coderSatya <sprakash6233@gmail.com>
2023-10-29 18:10:54 +0530

    queries

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 // import { BrowserRouter as Router,Route,Switch, Link, Routes } from 'react-router-dom'
-import SuperHeroesPage from './components/SuperHeroes.page';
-import RQSuperHeroesPage from './components/RQSuperHeroes.page';
-import HomePage from './components/Home.page';
-import { Routes,BrowserRouter,Route, Link } from 'react-router-dom';
-import { QueryClientProvider, QueryClient } from 'react-query';
+import SuperHeroesPage from "./components/SuperHeroes.page";
+import RQSuperHeroesPage from "./components/RQSuperHeroes.page";
+import HomePage from "./components/Home.page";
+import { Routes, BrowserRouter, Route, Link } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
 
-import { ReactQueryDevtools } from 'react-query/devtools'
+import { ReactQueryDevtools } from "react-query/devtools";
+import RQSuperHeroPage from "./components/RQSuperHero.page";
+import ParallelQueriesPage from "./components/ParallelQueries.page";
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-  
-    <div className="App">
-    <BrowserRouter>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/super-heroes'>Traditional Super Heroes</Link>
-          </li>
-          <li>
-            <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path='/super-heroes' element={<SuperHeroesPage />} />
-         
-        <Route path='/rq-super-heroes' element={<RQSuperHeroesPage/>} />
-          
-      
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </div>
-    
-  </BrowserRouter>
-    </div>
-    <ReactQueryDevtools intialIsOpen ={false} position="bottom-right" />
+      <div className="App">
+        <BrowserRouter>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/super-heroes">Traditional Super Heroes</Link>
+                </li>
+                <li>
+                  <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+                </li>
+              </ul>
+            </nav>
+            <Routes>
+              <Route path="/super-heroes" element={<SuperHeroesPage />} />
+              <Route path="/rq-parallel" element={<ParallelQueriesPage />} />
+              <Route
+                path="/rq-super-heroes/:heroId"
+                element={<RQSuperHeroPage />}
+              />
+              <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
+      <ReactQueryDevtools intialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
 }
--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,63 +1,112 @@
-import React from 'react';
-import { useQuery } from 'react-query';
-import axios from 'axios';
+// import React from 'react';
+// import { useQuery } from 'react-query';
+// import axios from 'axios';
 
-const fetchSuperHeroes = ()=>{
-  return axios.get('http://localhost:4000/superheroes')
-}
+// const fetchSuperHeroes = ()=>{
+//   return axios.get('http://localhost:4000/superheroes')
+// }
+
+// const RQSuperHeroesPage = () => {
+
+// //   const {isLoading, data} = useQuery('super-heroes', ()=>{
+// // return axios.get('http://localhost:4000/superheroes')
+// //   })  // 1st Method
+
+// const onSuccess = (data)=>{
+//   console.log('Perform side effects after data fetching', data)
+// }
+// const onError = (error)=>{
+//   console.log('Perform side effect after encountering error',error)
+// }
+
+// const {isLoading, data, isError, error, isFetching, refetch} = useQuery('super-heroes',
+// fetchSuperHeroes,
+// {
+//   // staleTime:0
+//   // refetchOnMount:true,
+//   // refetchOnWindowFocus:false
+//   // refetchInterval:false,
+//   // refetchIntervalInBackground:true
+//   //  enabled:false
+//   onSuccess:onSuccess,
+//   onError:onError,
+//   select:(data)=>{const superHeroNames = data.data.map((hero)=>hero.name)
+//   return superHeroNames
+// },
+// }
+// )
+// console.log({isFetching, isLoading})
+
+//   if(isLoading){
+//     return <h2>Loading...</h2>
+//   }
+//   if(isError){
+//     return <h2>{error.message}</h2>
+//   }
+
+//   // console.log(data.data, 'data')
+//   return (
+//     <div>
+//       <h1>RQ super hero</h1>
+//       <button onClick={refetch}>Show Heroes</button>
+
+// {data.map((heroName)=>{
+//   return <div key={heroName}>{heroName}</div>
+// })}
+//     </div>
+//   )
+// }
+
+// export default RQSuperHeroesPage
+
+import React from "react";
+import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
+
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const RQSuperHeroesPage = () => {
+  //   const {isLoading, data} = useQuery('super-heroes', ()=>{
+  // return axios.get('http://localhost:4000/superheroes')
+  //   })  // 1st Method
 
+  const onSuccess = (data) => {
+    console.log("Perform side effects after data fetching", data);
+  };
+  const onError = (error) => {
+    console.log("Perform side effect after encountering error", error);
+  };
 
+  const { isLoading, data, isError, error, isFetching, refetch } =
+    useSuperHeroesData(onSuccess, onError);
 
+  console.log({ isFetching, isLoading });
 
-//   const {isLoading, data} = useQuery('super-heroes', ()=>{
-// return axios.get('http://localhost:4000/superheroes')
-//   })  // 1st Method
-
-const onSuccess = (data)=>{
-  console.log('Perform side effects after data fetching', data)
-}
-const onError = (error)=>{
-  console.log('Perform side effect after encountering error',error)
-}
-
-const {isLoading, data, isError, error, isFetching, refetch} = useQuery('super-heroes',
-fetchSuperHeroes, 
-{
-  // staleTime:0
-  // refetchOnMount:true,
-  // refetchOnWindowFocus:false
-  // refetchInterval:false,
-  // refetchIntervalInBackground:true
-  //  enabled:false
-  onSuccess:onSuccess,
-  onError:onError,
-  select:(data)=>{const superHeroNames = data.data.map((hero)=>hero.name)
-  return superHeroNames
-},
-}
-) 
-console.log({isFetching, isLoading})
-
-  if(isLoading){
-    return <h2>Loading...</h2>
+  if (isLoading) {
+    return <h2>Loading...</h2>;
   }
-  if(isError){
-    return <h2>{error.message}</h2>
+  if (isError) {
+    return <h2>{error.message}</h2>;
   }
-  
+
   // console.log(data.data, 'data')
   return (
     <div>
       <h1>RQ super hero</h1>
       <button onClick={refetch}>Show Heroes</button>
-       
-{data.map((heroName)=>{
-  return <div key={heroName}>{heroName}</div>
-})}
-    </div>
-  )
-}
 
-export default RQSuperHeroesPage
+      {/* {data.map((heroName) => {
+        return <div key={heroName}>{heroName}</div>;
+      })} */}
+      {data?.data.map((hero) => {
+        return (
+          <div key={hero.id}>
+            <Link to={`/rq-super-heroes/${hero.id}`}>{hero.name}</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default RQSuperHeroesPage;
